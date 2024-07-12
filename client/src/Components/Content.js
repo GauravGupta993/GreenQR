c7cb0efc6cab5ec703822d2fb295c965be152ce7
GauravGupta993 <gaurav115993@gmail.com>
2024-07-12 17:54:40 +0530

    Fetch the data from backend.

--- client/src/Components/Content.js
+++ client/src/Components/Content.js
@@ -1,15 +1,34 @@
-import * as React from "react";
+// import React,  from "react";
+import React,{ useState, useEffect }  from "react";
 import { useLocation } from "react-router-dom";
 
 function Content() {
-    const location = useLocation();
-    console.log(location);
-    console.log('here');
-    // alert('here');
+  const location = useLocation();
+  console.log(location);
+  const [Data, setData] = useState('');
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://gardens-project.onrender.com/api/${location.state.id}`, {
+        method: 'GET'
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+      setData(data.scientific_name);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  useEffect(() => {
+    alert("here");
+    fetchData();
+  }, []);
   return (
     <>
-    <div>Home</div>
-    <div>{location.state.id}</div>
+      <div>Home</div>
+      <div>{Data}</div>
     </>
   );
 }
