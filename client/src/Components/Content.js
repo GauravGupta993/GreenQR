e8350fb3fe521472d2e0e5cc41c52ec03bdd432b
GauravGupta993 <gaurav115993@gmail.com>
2024-07-23 18:16:42 +0530

    Made changes in display content.

--- client/src/Components/Content.js
+++ client/src/Components/Content.js
@@ -2,7 +2,11 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import Box from "@mui/material/Box";
-
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 function Content() {
   const location = useLocation();
   console.log(location);
@@ -29,16 +33,54 @@ function Content() {
     // alert("here");
     fetchData();
   }, []);
+
+  const card = (
+    <React.Fragment>
+      <CardContent>
+        <Typography variant="h5" component="div">
+        {Data.scientific_name}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          (Scientific Name)
+        </Typography>
+        <Typography variant="h5" component="div">
+        {Data.common_name}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          (Common Name)
+        </Typography>
+        <Typography variant="h5" component="div">
+        {Data.flowering_time}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          (Flowering Time)
+        </Typography>
+        <Typography variant="h5" component="div">
+        {Data.identifying_characters}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          (Identifying Characters)
+        </Typography>
+        <Typography variant="h5" component="div">
+        {Data.economic_importance}
+        </Typography>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          (Economic Importance)
+        </Typography>
+
+      </CardContent>
+    </React.Fragment>
+  );
   return (
     <>
-      <div>Home</div>
-      <div>{Data.scientific_name}</div>
+      {/* <div>Home</div> */}
+      {/* <div>{Data.scientific_name}</div>
       <div>{Data.common_name}</div>
       <div>{Data.flowering_time}</div>
       <div>{Data.identifying_characters}</div>
-      <div>{Data.economic_importance}</div>
+      <div>{Data.economic_importance}</div> */}
 
-      <Box component="div" sx={{ whiteSpace: "normal" }}>
+      {/* <Box component="div" sx={{ whiteSpace: "normal" }}>
       {Data.scientific_name}
       </Box>
       <Box component="div" sx={{ whiteSpace: "normal" }}>
@@ -52,6 +94,9 @@ function Content() {
       </Box>
       <Box component="div" sx={{ whiteSpace: "normal" }}>
       {Data.economic_importance}
+      </Box> */}
+      <Box sx={{ minWidth: 275, mx:10 }}>
+        <Card variant="outlined">{card}</Card>
       </Box>
     </>
   );
